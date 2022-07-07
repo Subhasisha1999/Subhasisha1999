03acfc528b4cf9b5c7683b863e34c5ee9d0cc3bf
Subhasisha1999 <87237165+Subhasisha1999@users.noreply.github.com>
2022-07-07 18:01:08 +0530

    PRINT NUMBER ENTER BY USER

---
+++ printnumberenterbyuser.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the value of a= ";
+    cin>>a;
+    cout<<"enter the value of a= ";
+    cin>>b;
+    cout<<endl<<"the value of a:"<<a;
+     cout<<endl<<"the value of b:"<<b;
+    return 0;
+}
