efcaa55c82423186c2089a66ae584ddb4fbbf670
Subhasisha1999 <87237165+Subhasisha1999@users.noreply.github.com>
2022-07-07 18:20:39 +0530

    FIND REMENDER AND QUOTIENT

---
+++ findremandquo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    cout<<"enter the value of x and y"<<endl;
+    cin>>x>>y;
+    cout<<"the remender is when x is divided with y : "<<x%y<<endl;
+    cout<<"the quotient is when x is divided with y : "<<x/y;
+    return 0;
+}
