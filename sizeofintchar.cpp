6f74e3626ab97892d1f965df8d18ae8d48a5e6dd
Subhasisha1999 <87237165+Subhasisha1999@users.noreply.github.com>
2022-07-07 18:30:50 +0530

    SIZE OF INT,CHAR,......

---
+++ sizeofintchar.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    char b;
+    double c;
+    float d;
+    long long e;
+    cout<<"size  of integer : "<<sizeof(a)<<endl;
+    cout<<"size  of  char : "<<sizeof(b)<<endl;
+    cout<<"size  of double : "<<sizeof(c)<<endl;
+    cout<<"size  of float : "<<sizeof(d)<<endl;
+    cout<<"size  of long long : "<<sizeof(e)<<endl;
+    //cout<<"size  of integer : "<<sizeof(a);
+    return 0;
+}
