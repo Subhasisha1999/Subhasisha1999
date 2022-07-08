0b7166d10d4d45e32dbfa4fbede25ddf343cd821
Subhasisha1999 <87237165+Subhasisha1999@users.noreply.github.com>
2022-07-08 11:49:15 +0530

    CHECK A NUMBER POSITIVE OR NEGATIVE

---
+++ positiveandnegative.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter the value of a : "<<endl;
+    cin>>a;
+    if(a>0){
+        cout<<" POSITIVE "<<endl;
+    }
+    else{
+        cout<<" NEGATIVE "<<endl;
+    }
+    return 0;
+
+}
