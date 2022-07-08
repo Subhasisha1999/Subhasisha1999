2439efcdb862ac98b2983d172150409767010b4f
Subhasisha1999 <87237165+Subhasisha1999@users.noreply.github.com>
2022-07-08 11:44:32 +0530

    COMPARE TWO NUMBERS

---
+++ comparetwonumbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the value of a : "<<endl;
+    cin>>a;
+    cout<<"enter the value of b : "<<endl;
+    cin>>b;
+    if(a<b){
+        cout<<" the value of b is greater "<<endl;
+    }
+    else{
+        cout<<" the value of a is greater  "<<endl;
+    }
+    return 0;
+
+}
