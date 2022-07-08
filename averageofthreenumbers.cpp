ff03225bab31203843b317dab04d2662cf0e794f
Subhasisha1999 <87237165+Subhasisha1999@users.noreply.github.com>
2022-07-08 11:43:35 +0530

    AVERAGE OF THREE NUMBERS

---
+++ averageofthreenumbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter the value of a : "<<endl;
+    cin>>a;
+    cout<<"enter the value of b : "<<endl;
+    cin>>b;
+    cout<<"enter the value of c : "<<endl;
+    cin>>c;
+    int avg =(a+b+c)/3;
+    cout<<"the average of the three numbers are : "<< avg;
+    return 0;
+
+}
