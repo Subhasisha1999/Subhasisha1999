39955451b44b7f917f6ac648d994e1e74eab27e2
Subhasisha1999 <87237165+Subhasisha1999@users.noreply.github.com>
2022-07-08 11:45:56 +0530

    PRINT NATURAL NUMBERS

---
+++ numbersprint.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    cout<<"enter the value of n  : "<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        cout<< i <<" ";
+    }
+    return 0;
+}
