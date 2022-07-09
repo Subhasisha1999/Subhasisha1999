466e9b68ba36f90b8b8ea02c1e7a6cfa2e947fdd
Subhasisha1999 <87237165+Subhasisha1999@users.noreply.github.com>
2022-07-09 19:26:25 +0530

    REVERSE INVERTED HALF TRIANGLE

---
+++ revinvertedlefthalftriangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,k,j,n;
+    cout<<"enter the value for n : "<<endl;
+    cin>>n;
+    for(i=0;i<n;i++){
+        for(j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(j=0;j<n-i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }   
+    return 0;
+}
